67c54dc23746c5d9d7f840e49d441b0128d468d3
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2014-03-28 11:31:02 +0100

    Test for feature flip + on build server

--- test/unit/feature-flip.js
+++ test/unit/feature-flip.js
@@ -9,26 +9,24 @@ var feature = require('../../utils/feature-flip');
 describe('Feature Flip', function() {
 
     describe('feature({name}).isEnabled', function() {
+        var env = process.env.NODE_ENV;
 
-        var status;
-
-        before(function() {
-            status = feature('test-feature').isEnabled;
-        });
-
-        before(function() {
-            if (status) {
-                feature.enable('test-feature');
-            } else {
-                feature.disable('test-feature');
-            }
+        afterEach(function() {
+            process.env.NODE_ENV = env;
         });
 
         it('should disable all features not listen in configuration', function() {
+            process.env.NODE_ENV = 'production';
             expect(feature('not-a-valid-feature').isEnabled).to.be.false;
         });
 
+        it('should enable all features on build server', function() {
+            process.env.NODE_ENV = 'build';
+            expect(feature('not-a-valid-feature').isEnabled).to.be.true;
+        });
+
         it('should return the status of features', function() {
+            process.env.NODE_ENV = 'production';
             feature.enable('test-feature');
             expect(feature('test-feature').isEnabled).to.be.true;
 
